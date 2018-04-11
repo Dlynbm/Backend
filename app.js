f234b0ce0a40e94d98f35c779759035bffdee823
iamdlynm <dbum.35@hotmail.com>
2018-04-11 07:46:28 -0600

    First commit

--- app.js
+++ app.js
@@ -1,9 +1,35 @@
-app.set('views', path.join(__dirname, 'views'));
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const path = require('path');
+
+app.use(express.static('public'));
 app.set('view engine', 'pug');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 
 
 
-app.get('/', (req, res) => {
-    res.render('index', {
-    list: 'DLyn'
-});}
+app.get('/greetings/:title', function(req, res){
+    const title = req.params.title;
+    //res.render();
+});
+
+
+app.post('/new-user', function(req, res){
+    const userid = req.body.userid;
+    const firstName = req.body.firstname;
+    const lastName = req.body.lastname;
+    const email = req.body.email;
+    const age = req.body.age;
+    console.log(req.body);
+    res.render('confirm', {userid: userid, firstName:firstName, lastName:lastName, email:email, age: age });
+});
+
+function goBack() {
+    window.history.back();
+}
+app.listen(4200, function() {
+    console.log('the app is running');
+});
+
