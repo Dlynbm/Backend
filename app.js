723a6d03367be9e52288b46235c6d96a058d370a
iamdlynm <dbum.35@hotmail.com>
2018-04-16 11:21:17 -0600

    Added edit view

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 let allUsers =[];
+let user;
 
 app.use(express.static('public'));
 app.set('view engine', 'pug');
@@ -16,13 +17,13 @@ app.get('/new-user', function(req, res){
 });
 
 
-app.get('/delete/:allUsers', (req, rest) => {
-    for(let i = 0; i < allUsers.length; i++){
-        if(allUsers[i].id == req.params.userid) {
-            allUsers.splice(i,1);
-        }
-    }
-});
+//app.get('/delete/:allUsers', (req, rest) => {
+    //for(let i = 0; i < allUsers.length; i++){
+        //if(allUsers[i].id == req.params.userid) {
+            //allUsers.splice(i,1);
+        //}
+    //}
+//});
 
 
 app.post('/new-user', function(req, res){
@@ -35,6 +36,33 @@ app.post('/new-user', function(req, res){
     allUsers.push(individualUser);
     console.log(req.body);
     res.render('confirm', {users: allUsers });
+});
+
+app.get('/edit/:id', (req, res) => {
+    console.log('edit user' + req.params.id);
+    let userInfo;
+    for(let i = 0; i < allUsers.length; i++){
+        if(req.params.id === allUsers[i].userid){
+            userInfo = allUsers[i];
+            res.render('./edit', {user: userInfo});
+        }
+    }
+});
+
+app.post('/edit', (req, res) => {
+    let userEdit = {
+        userid: req.body.userid,
+        name: req.body.name,
+        email: req.body.email,
+        age: req.body.age,
+        id: req.body.id
+    };
+    for(let i = 0; i < allUsers.length; i++){
+        if(+req.body.id === allUsers[i].id){
+            allUsers[i] = userEdit;
+        }
+    }
+    //res.render('users', {users:allUsers});
 });
 
 
